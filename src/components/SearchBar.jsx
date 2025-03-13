0cc1c1a9d7aada5ec578f3ac76fcd3cbf3d8ff70
danniel917 <danniel917@hotmail.com>
2025-03-12 20:17:34 -0500

    add state to search items

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const sortOptions = {
   "Best Match": "best_match",
@@ -7,11 +7,38 @@ const sortOptions = {
 };
 
 function SearchBar() {
+  const [search, setSearch] = useState("");
+  const [location, setLocation] = useState("");
+  const [selectOption, setSelectOption] = useState("");
+
+  const handleOptionClick = (option) => {
+    setSelectOption(sortOptions[option]);
+  };
+
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    console.log(`Searching Yelp with ${search}, ${location}, ${selectOption} `);
+  };
+
   return (
     <div className="relative flex justify-center flex-col gap-8 bg-gray-800 px-16 py-48 bg-[url(https://images.pexels.com/photos/1322184/pexels-photo-1322184.jpeg)] bg-center bg-cover text-white before:absolute before:content-[''] before:bg-black before:opacity-50 before:inset-0 before:z-10">
       <div className="relative flex justify-center gap-4 z-10">
         {Object.keys(sortOptions).map((option) => (
-          <button key={option}>{option}</button>
+          <button
+            id={option}
+            key={option}
+            onClick={() => handleOptionClick(option)}
+            className={`
+            ${
+              sortOptions[option] === selectOption
+                ? "bg-yellow-400 text-black"
+                : "bg-gray-400 text-white"
+            }
+            px-4 py-2 rounded cursor-pointer
+            `}
+          >
+            {option}
+          </button>
         ))}
       </div>
       <div className="relative z-10 flex justify-center gap-4">
@@ -19,14 +46,21 @@ function SearchBar() {
           className="bg-gray-200 text-black placeholder:text-gray-400 px-4 py-2 rounded"
           type="text"
           placeholder="Search Businesses"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
         <input
           className="bg-gray-200 text-black placeholder:text-gray-400 px-4 py-2 rounded"
           type="text"
           placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
         />
       </div>
-      <button className="relative z-10 rounded bg-yellow-400 text-black px-4 py-2 inline-block mx-auto cursor-pointer">
+      <button
+        className="relative z-10 rounded bg-yellow-400 text-black px-4 py-2 inline-block mx-auto cursor-pointer"
+        onClick={(e) => handleButtonClick(e)}
+      >
         Search
       </button>
     </div>
