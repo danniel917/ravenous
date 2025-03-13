b24d321684dd235be8a0ffc53bc7d2953b74a6e9
danniel917 <danniel917@hotmail.com>
2025-03-12 19:23:12 -0500

    Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import BusinessList from "./components/BusinessList";
+import { businesses } from "./helpers/businesses";
+
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl text-orange-300">Ravenous</h1>
+      <h1 className="text-6xl text-center uppercase mb-2">Ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses} />
     </>
   );
 }
---
+++ src/helpers/businesses.js
@@ -0,0 +1,41 @@
+export const businesses = [
+    {
+      id: 1,
+      imageSrc:
+        "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+      name: "MarginOtto Pizzeria",
+      address: "1010 Paddington Way",
+      city: "Flavortown",
+      state: "NY",
+      zipCode: "10101",
+      category: "Italian",
+      rating: 4.5,
+      reviewCount: 90,
+    },
+    {
+      id: 2,
+      imageSrc:
+        "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+      name: "Pasta Palace",
+      address: "456 Elm St",
+      city: "Los Angeles",
+      state: "CA",
+      zipCode: "90001",
+      category: "Italian",
+      rating: 4.7,
+      reviewCount: 120,
+    },
+    {
+      id: 3,
+      imageSrc:
+        "https://content.codecademy.com/programs/react/ravenous/pizza.jpg",
+      name: "Sushi Spot",
+      address: "789 Oak St",
+      city: "San Francisco",
+      state: "CA",
+      zipCode: "94103",
+      category: "Sushi",
+      rating: 4.8,
+      reviewCount: 200,
+    },
+  ];
